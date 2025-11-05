a087a4c04edc0b3ae40ad4e2e89a8309fe15874a
yashdhameliya28 <140818500+yashdhameliya28@users.noreply.github.com>
2025-11-05 13:19:12 +0530

    Date:5-11-25

--- Lab_2.java
+++ Lab_2.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class Lab_2 {
     public static void main(String[] args) {
         printEvenNumBetween2Num(1, 10);
@@ -112,3 +113,119 @@ public class Lab_2 {
     }
 
 }
+=======
+public class Lab_2 {
+    public static void main(String[] args) {
+        printEvenNumBetween2Num(1, 10);
+        printFactor(12);
+        System.out.println(sumOfDivisor(12));
+        System.out.println(GCD(2, 4));
+        System.out.println(LCM(8, 12));
+        if(isPerfectNum(6)){
+            System.out.println("Given number is perfect number");
+        }
+        else{
+            System.out.println("Given number is not perfect number");
+        }
+
+        if(isHarshadNum(18)){
+            System.out.println("Given number is harshad number");
+        }else{
+            System.out.println("Given number is not harshad number");
+        }
+
+        if(isPrime(5)){
+            System.out.println("Given number is prime number");
+        }else{
+            System.out.println("Given number is not prime number");
+        }
+    }
+
+    public static void printEvenNumBetween2Num(int n, int m){
+        for(int i=n; i<=m; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void printFactor(int n){
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int sumOfDivisor(int n){
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+
+    public static int GCD(int x, int y){
+        int GCD = 0;
+        int min = (x>y) ? y : x;
+        for(int i=1; i<=min; i++){
+            if(x%i==0 && y%i==0){
+                GCD = i;
+            }
+        }
+        return GCD;
+    }
+
+    public static int LCM(int x, int y){
+        int LCM = 1; 
+        int max = (x>y) ? x :y;
+        for(int i=max; i<=(x*y); i++){
+            if(i%x==0 && i%y==0){
+                LCM = i;
+                break;
+            }
+        }
+        return LCM;
+    }
+
+    public static boolean isPerfectNum(int n){
+        int sum=0;
+        for(int i=1; i<n; i++){
+            if(n%i==0){
+                sum += i;
+            }
+        }
+        if(sum!=n){
+            return false;
+        }
+        return true;
+    }
+
+    
+    public static boolean isHarshadNum(int n){
+        int sum=0, rem=0, temp = n;
+        while (temp!=0) {
+            rem = temp % 10;
+            sum+=rem;
+            temp/=10;
+        }
+        if(n%sum!=0){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isPrime(int n){
+        if(n <= 1) return false;
+        for(int i=2; i<=(n/2); i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+>>>>>>> f9760b9 (Date:5-11-25)
--- Lab_3.java
+++ Lab_3.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class Lab_3 {
     public static void main(String[] args) {
         System.out.println(sumOfSequence(3));
@@ -130,3 +131,137 @@ public class Lab_3 {
         
     }
 }
+=======
+public class Lab_3 {
+    public static void main(String[] args) {
+        System.out.println(sumOfSequence(3));
+        // System.out.println(powerWithoutMiltiplaction(2, 3));
+    }   
+    
+    public static int sumOfSequence(int n){
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                sum+=j;
+            }
+        }
+        return sum;
+    }
+
+    public class PowerWithoutMulti {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 3;
+        int ans = 1;
+
+        for (int i = 0; i < y; i++) {
+            ans = multiply(ans, x);
+        }
+
+        System.out.println("Power is: " + ans);
+    }
+
+    static int multiply(int a, int b) {
+        int result = 0;
+        for (int i = 0; i < b; i++) {
+            result += a;
+        }
+        return result;
+    }
+}
+
+
+    public static int findLargestElement(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    } 
+
+    public static int findSmallestElement(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int FindElementInArr(int[] arr, int k){
+        if(arr.length == 0) return 0; 
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == k){
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void reverseArr(int[] arr){
+        for(int i=0; i<(arr.length)/2; i++){
+            int temp = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp; 
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print("Reverse array "+arr[i]);
+        }
+    }
+
+    public static void printElement(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            if((arr[i]+arr[i+1])%2 == 0){
+                System.out.println(i + " " + (i+1));
+            }
+        }
+    }
+
+    public static void findDuplicateElement(int[] arr){
+        int dcount=0;
+        for(int i=0; i<arr.length; i++){
+            int temp=arr[i];
+            for(int j=1; j<arr.length; j++){
+                if(temp == arr[j]){
+                    dcount++;
+                }
+            }
+            if(dcount!=1){
+                System.out.println("duplicate element : "+arr[i]);
+            }
+        }
+    }
+
+    public static void primeNumBetweenTwoNum(int n, int m){
+       for(int i=n; i<=m; i++){
+            int temp = i;
+            for(int j=2; j<=(temp)/2; j++){
+                if(temp%i==0){
+                    System.out.println(temp+"is not prime number");
+                }
+                else{
+                    System.out.println(temp+"is prime number");
+                }
+            }
+       } 
+    }
+
+    public static void frequencyOfEachElement(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            int count=0;
+            int temp=arr[i];
+            for(int j=1; j<arr.length; j++){
+                if(temp == arr[i]) count++;
+            }
+            System.out.println("Element is"+arr[i]+"and it's frequency is"+count);
+        }
+    }
+
+    public static void findSecondLargestElement(int[] arr){
+        
+    }
+}
+>>>>>>> f9760b9 (Date:5-11-25)
