4afbb9bf86f81b4ad279b1e2c12fc2cfecaa882b
yashdhameliya28 <140818500+yashdhameliya28@users.noreply.github.com>
2025-11-09 17:14:21 +0530

    Date:9-11-25

--- Lab_6.java
+++ Lab_6.java
@@ -1,6 +1,20 @@
 public class Lab_6 {
     public static void main(String[] args) {
-        patterrn1(4);
+        // patterrn1(4);
+        // printPascalTriangle();
+        // pattern19();
+        int[] arr = { 2, 1, 5, 4 };
+        int[] arr1 = { 2, 1, 5, 4 };
+        // printArr(insertionSort(arr));
+        // printArr(selectionSort(arr1));
+        int[] arr2 = {1,2,3,4,5,6,7};
+        // printArr(rotateArr(arr2, 3)); 
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
     // if n = 5
@@ -23,10 +37,10 @@ public class Lab_6 {
         }
     }
 
-    //1
-    //1*1
-    //1*3*1
-    //1*3*5*3*1
+    // 1
+    // 1*1
+    // 1*3*1
+    // 1*3*5*3*1
     public static void pattern12() {
         int n = 4;
         for (int i = 1; i <= n; i++) {
@@ -43,11 +57,11 @@ public class Lab_6 {
     }
 
     // * * * * * * *
-    //   *        *
-    //     *   *
-    //       * 
-    //     *    *
-    //   *        *
+    // * *
+    // * *
+    // *
+    // * *
+    // * *
     // * * * * * * * *
     public static void pattern13(int n) {
         int i, j;
@@ -78,37 +92,37 @@ public class Lab_6 {
         }
     }
 
-        //     1
-        //    212
-        //    32123
-        //   4321234
-        //    32123
-        //     212
-        //      1
+    // 1
+    // 212
+    // 32123
+    // 4321234
+    // 32123
+    // 212
+    // 1
 
-    public static void pattern14(){
-         int n = 4;   
-        for(int i = 1; i <= n; i++){
-            for(int s = n; s > i; s--){
+    public static void pattern14() {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int s = n; s > i; s--) {
                 System.out.print(" ");
             }
-            for(int j = i; j >= 1; j--){
+            for (int j = i; j >= 1; j--) {
                 System.out.print(j);
             }
-            for(int j = 2; j <= i; j++){
+            for (int j = 2; j <= i; j++) {
                 System.out.print(j);
             }
 
             System.out.println();
         }
-        for(int i = n-1; i >= 1; i--){
-            for(int s = n; s > i; s--){
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = n; s > i; s--) {
                 System.out.print(" ");
             }
-            for(int j = i; j >= 1; j--){
+            for (int j = i; j >= 1; j--) {
                 System.out.print(j);
             }
-            for(int j = 2; j <= i; j++){
+            for (int j = 2; j <= i; j++) {
                 System.out.print(j);
             }
 
@@ -116,14 +130,14 @@ public class Lab_6 {
         }
     }
 
-//     1
-//     1 2 1
-//     1 * 3 * 1
-//     1 * * 4 * * 1
-//     1 * * * 5 * * * 1
-//     1 * * * * 6 * * * *1
-    public static void pattern15(){
-         int n = 6;
+    // 1
+    // 1 2 1
+    // 1 * 3 * 1
+    // 1 * * 4 * * 1
+    // 1 * * * 5 * * * 1
+    // 1 * * * * 6 * * * *1
+    public static void pattern15() {
+        int n = 6;
         for (int i = 1; i <= n; i++) {
             System.out.print("1 ");
             for (int j = 1; j < i - 1; j++) {
@@ -141,4 +155,123 @@ public class Lab_6 {
             System.out.println();
         }
     }
+
+    public static void printPascalTriangle() {
+        int n = 6;
+        for (int i = 0; i < n; i++) {
+            for (int s = 0; s < n - i; s++) {
+                System.out.print(" ");
+            }
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern19() {
+        int n = 5, temp = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print(temp + " ");
+                }
+            } else {
+
+            }
+            System.out.println();
+        }
+    }
+
+    // 4444444
+    // 4333334
+    // 4322234
+    // 4321234
+    // 4322234
+    // 4333334
+    // 4444444
+    public static void Patten20() {
+        int n = 4;
+        int size = 2 * n - 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int right = size - 1 - j;
+                int bottom = size - 1 - i;
+
+                int min = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+                System.out.print(n - min);
+            }
+            System.out.println();
+        }
+    }
+
+    // 12345
+    // 109876
+    // 1112131415
+    // 1617181920
+    // 2122232425
+    public static void Patten19() {
+
+        int n = 5;
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            int rowStart = num;
+            num += n;
+            if (i % 2 == 1) {
+                for (int j = rowStart; j < rowStart + n; j++) {
+                    System.out.print(j + " ");
+                }
+            } else {
+                for (int j = rowStart + n - 1; j >= rowStart; j--) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+
+    public static int[] selectionSort(int[] arr) {
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    // public static int[] rotateArr(int[] arr, int k){
+    //     for(int i=1; i<=k; i++){
+    //         int temp = arr[arr.length-i];
+    //         for(int j=arr.length-1; j>0; j--){
+    //             arr[j] = arr[j];
+    //         }
+    //         arr[i] = temp;
+    //     }
+    //     return arr;
+    // }
 }
